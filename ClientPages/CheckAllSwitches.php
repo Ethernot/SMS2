f10c71e7ae6db25f1c695b5105265c25c4b174d8
Ricardo <ricardopintokk@hotmail.com>
2014-12-30 17:59:43 +0000

    control panel v1

--- ClientPages/CheckAllSwitches.php
+++ ClientPages/CheckAllSwitches.php
@@ -15,15 +15,19 @@
     <?php
     require_once("../Server/Database.php");
     $db = new Database();
-    $info = $db->getAllSwitchesName();
+//    $info = $db->getAllSwitchesName();
+    $info = "11,222,333,4,555,6666";
     $switchesNameList = explode(",",$info);
     error_reporting(0);
 
     error_reporting(1);
-    echo "<h1>All switches: </h1>";
+    echo "<h1>Control Panel: all switches: </h1>";
     foreach($switchesNameList as $a){
         echo "<form action='CheckAllSwitches.php' method='post'>";
-
+        echo "<label>Switch name: ".$a."</label>";
+        if($db->isSwichtEn)
+        echo "<input type='radio' name='alter' value='Enable'>";
+        echo "<input type='radio' name='alter' value='Disable'>";
         echo "</form>";
     }
     ?>
