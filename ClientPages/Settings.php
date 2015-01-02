bafd2a20e281a1c3534d270fb037d179805c6813
Ricardo <ricardopintokk@hotmail.com>
2015-01-02 18:32:47 +0000

    TODO update settings v1.0 Server?

--- ClientPages/Settings.php
+++ ClientPages/Settings.php
@@ -11,9 +11,20 @@
     <span>**Switch Management Software**</span>
 </header>
 
+<div class="mainContainer">
+<h1>Settings:</h1>
+    <?php
+    require_once("../Server/Database.php");
+    $db = new Database();
+//    echo "<label>Server get Configurations interval: ".db->getKKKKK." days </label>";
+    echo "<form id='f' action='Settings.php' method='post'>";
+    echo "<label> update interval: </label>";
+    echo "</form>";
+    ?>
 
-<br>
+    <br>
 <button onclick="goBack()">Go Back</button>
+</div>
 
 <script>
     function goBack() {
